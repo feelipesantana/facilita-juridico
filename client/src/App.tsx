8fedcc333404dd520e0ea1ab71333841cad736ec
Felipe Santana <felipe.jesus0801@gmail.com>
2024-01-19 15:23:03 -0300

    feat: frontend

---
+++ client/src/App.tsx
@@ -0,0 +1,21 @@
+import { ModeToggle } from "./components/ModeToogle"
+import { TableClients } from "./components/TableClients"
+
+
+function App() {
+
+
+  return (
+    <main className=' p-10  h-screen w-screen'>
+      <header className="flex justify-between items-center border-b h-20 mb-20">
+        <h1 className=" font-bold text-3xl ">Facilita Jurídico</h1>
+        <ModeToggle />
+      </header>
+
+      <TableClients />
+
+    </main>
+  )
+}
+
+export default App
