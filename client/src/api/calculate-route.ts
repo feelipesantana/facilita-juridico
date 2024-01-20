c118254fc2e09c66e8858fe6dd7093d9cc7f9b02
Felipe Santana <felipe.jesus0801@gmail.com>
2024-01-20 11:16:23 -0300

    feat: route

--- client/src/api/calculate-route.ts
+++ client/src/api/calculate-route.ts
@@ -1,6 +1,12 @@
-import { useQueryClient } from "@tanstack/react-query";
 import { api } from "../services/axios";
 
+export interface CalculateRouteProps{
+    id: number
+    x: number;
+    y: number;
+    name: string;
+    id_client: string;
+}
 export async function calculateRoute(){
 
     const response = await api.get('/calc-route')
--- client/src/components/Modals/ModalRoute.tsx
+++ client/src/components/Modals/ModalRoute.tsx
@@ -1,10 +1,10 @@
 import { useQuery } from "@tanstack/react-query";
 import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle, DialogTrigger } from "../ui/dialog";
-import { calculateRoute } from "../../api/calculate-route";
+import { CalculateRouteProps, calculateRoute } from "../../api/calculate-route";
 import { ArrowRight } from "lucide-react";
 
 export function ModalRoute() {
-    const { data } = useQuery({
+    const { data } = useQuery<CalculateRouteProps[]>({
         queryKey: ['route'],
         queryFn: calculateRoute,
         refetchOnMount: true
@@ -13,21 +13,22 @@ export function ModalRoute() {
     return (
         <Dialog>
             <DialogTrigger>Sync melhor rota</DialogTrigger>
-            <DialogContent>
+            <DialogContent className="min-w-[600px] w-full">
                 <DialogHeader>
                     <DialogTitle>Rota</DialogTitle>
                     <DialogDescription className="mt-20">
                         Melhor rota syncronizada em ordem
                     </DialogDescription>
                 </DialogHeader>
-                <div className="flex flex-wrap gap-2 items-center justify-center my-10 p-2">
-                    {data?.map((res: any) => {
+                <div className="grid grid-cols-3 gap-2 items-center justify-center my-10 p-2">
+                    {data?.map((res, _index) => {
+
                         return (
                             <div className="flex items-center gap-2 justify-start ">
-                                <div key={res.id} className="bg-cyan-700 rounded-lg p-2  w-32 flex text-nowrap ">
+                                <div key={res.id} className="bg-slate-200 rounded-lg p-2  w-32 flex text-nowrap dark:bg-slate-600 ">
                                     <span className="flex no-wrap">{res.name}</span>
                                 </div>
-                                {data.lenght !== data.length - 1 && <ArrowRight className="min-h-4 min-w-4" />}
+                                {data.length - 1 !== _index && <ArrowRight className="min-h-4 min-w-4" />}
                             </div>
 
                         )
