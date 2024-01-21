4d86cda6843cdc6e01a0d580efb38f8a510f9458
Felipe Santana <felipe.jesus0801@gmail.com>
2024-01-21 11:23:01 -0300

    feat: setting to deploy

--- server/src/env.ts
+++
@@ -1,5 +0,0 @@
-import dotenv from "dotenv";
-import z from "zod";
-dotenv.config();
-
-export const envSchema = z.object({});
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,8 +1,10 @@
 import cors from "@fastify/cors";
+import env from "dotenv";
 import Fastify, { FastifyReply, FastifyRequest } from "fastify";
 import { appRoutes } from "./http/routes";
-
 const app = Fastify();
+
+env.config();
 
 app.register(cors, {
 	origin: "*",
@@ -13,7 +15,7 @@ app.register(appRoutes);
 
 app
 	.listen({
-		port: 3333,
+		port: process.env.PORT ? Number(process.env.PORT) : 3333,
 		host: "0.0.0.0",
 	})
 	.then(() => {
